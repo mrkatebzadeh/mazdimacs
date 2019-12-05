fb0626ac1ab969bc4ac9ec6823741971e2adb930
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2019-12-05 06:54:21 +0000

    Fixed path

--- modules/app/mk-twitter.el
+++ modules/app/mk-twitter.el
@@ -28,12 +28,12 @@
 (use-package twittering-mode
   :commands twit
   :defer t
+  :custom
+  (twittering-private-info-file (concat org-directory "/keys/twittering-mode.gpg"))
   :config
   (setq twittering-use-master-password t)
   (setq twittering-initial-timeline-spec-string
-	'(":home"
-	  ":replies"
-	  ":search/emacs/"))
+	'(":home"))
   (setq twittering-icon-mode t)
   (setq twittering-timer-interval 300)
   (setq twittering-url-show-status nil))
