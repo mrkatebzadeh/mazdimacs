4b1995e2e0f19376d964015c9f9f3db27014e7b1
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2025-04-16 13:55:10 +0000

    Fixed project issue

--- modules/editor/mazd-file.el
+++ modules/editor/mazd-file.el
@@ -46,13 +46,35 @@
 
   (use-package project
     :init
-    (defun mazd//project-override (dir)
-      (let ((override (locate-dominating-file dir ".project.el")))
-	(if override
-	    (cons 'vc override)
-	  nil)))
     :config
-    (add-hook 'project-find-functions #'mazd//project-override))
+    (defgroup project-local nil
+      "Local, non-VC-backed project.el root directories."
+      :group 'project)
+
+    (defcustom project-local-identifier ".project"
+
+      "You can specify a single filename or a list of names."
+      :type '(choice (string :tag "Single file")
+                     (repeat (string :tag "Filename")))
+      :group 'project-local)
+
+    (cl-defmethod project-root ((project (head local)))
+      "Return root directory of current PROJECT."
+      (cdr project))
+
+    (defun project-local-try-local (dir)
+      "Determine if DIR is a non-VC project. DIR must include a file with the name determined by the variable `project-local-identifier' to be considered a project."
+      (if-let ((root (if (listp project-local-identifier)
+			 (seq-some (lambda (n)
+                                     (locate-dominating-file dir n))
+				   project-local-identifier)
+                       (locate-dominating-file dir project-local-identifier))))
+	  (cons 'local root)))
+
+    (customize-set-variable 'project-find-functions
+                            (list #'project-try-vc
+				  #'project-local-try-local))
+    )
 
   (defun mazd//project-close ()
     "Close all buffers associated with the current project."
@@ -69,28 +91,28 @@
 
   )
 
-(use-package recentf
-  :ensure nil
-  :defer t
-  :init
-  (add-hook 'find-file-hook (lambda () (unless recentf-mode
-					 (recentf-mode)
-					 (recentf-track-opened-file))))
-  :custom
-  (recentf-exclude (list "COMMIT_EDITMSG"
-                         "~$"
-                         "/scp:"
-                         (expand-file-name mazd//backup-dir)
-                         (expand-file-name mazd//local-dir)
-                         (expand-file-name org-directory)
-                         (expand-file-name (concat mazd//emacs-dir "emms/"))
-                         "/ssh:"
-                         "/sudo:"
-                         "/tmp/"))
-  (recentf-max-menu-items 15)
-  (recentf-max-saved-items 15)
-  (recentf-save-file (concat mazd//backup-dir "recentf"))
-  :config (run-at-time nil (* 5 60) 'recentf-save-list))
+  (use-package recentf
+    :ensure nil
+    :defer t
+    :init
+    (add-hook 'find-file-hook (lambda () (unless recentf-mode
+					   (recentf-mode)
+					   (recentf-track-opened-file))))
+    :custom
+    (recentf-exclude (list "COMMIT_EDITMSG"
+                           "~$"
+                           "/scp:"
+                           (expand-file-name mazd//backup-dir)
+                           (expand-file-name mazd//local-dir)
+                           (expand-file-name org-directory)
+                           (expand-file-name (concat mazd//emacs-dir "emms/"))
+                           "/ssh:"
+                           "/sudo:"
+                           "/tmp/"))
+    (recentf-max-menu-items 15)
+    (recentf-max-saved-items 15)
+    (recentf-save-file (concat mazd//backup-dir "recentf"))
+    :config (run-at-time nil (* 5 60) 'recentf-save-list))
 
 (use-package docker-tramp
   :ensure t
